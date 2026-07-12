4a9e79d88c2c104e39282af722c40f47f4aa91cc
ranasamiafzal <ranasami0909@gmail.com>
2026-07-12 12:11:14 +0500

    refactor: remove commented-out legacy code and unused configurations across core application files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-// import React, { useEffect } from 'react'
-// import AppRoutes from './routes/AppRoutes'
-// import { useAuth } from './hooks/useAuth'
-
-// function App() {
-//   const { fetchUser } = useAuth();
-
-//   useEffect(() => {
-//     fetchUser();
-//   }, [location.pathname, fetchUser]);
-
-//   return <AppRoutes/>
-// }
-
-// export default App
-
 import React, { useEffect } from 'react';
 import AppRoutes from './routes/AppRoutes';
 import { useAuth } from './hooks/useAuth';
